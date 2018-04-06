e734db9fe411890507cd1ff8a73544aad08c5ca8
Steve Mokris <smokris@softpixel.com>
2018-04-05 20:51:14 -0400

    Update to latest LLVM package and switch to libc++.

--- conanfile.py
+++ conanfile.py
@@ -9,9 +9,9 @@ class FfmpegConan(ConanFile):
     package_version = '2'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-2@vuo/stable', \
-        'vuoutils/1.0@vuo/stable', \
-        'openssl/1.0.2n-2@vuo/stable'
+    build_requires = 'llvm/3.3-5@vuo/stable' \
+                     'vuoutils/1.0@vuo/stable'
+    requires = 'openssl/1.0.2n-2@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'http://www.ffmpeg.org/'
     license = 'http://www.ffmpeg.org/legal.html'
